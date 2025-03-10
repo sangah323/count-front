5a57e269e77c9e37c65bd774950c65a33d132239
imbhj <wnqudgus5565@gmail.com>
2025-03-11 08:23:42 +0900

    Fix: 250311 AWS 프론트엔드 코드 업로드

--- src/api/counter.js
+++ src/api/counter.js
@@ -1,11 +1,11 @@
 import instance from "./axios";
 
 export const getCount = async() => {
-    const { data } = await instance.get(`${process.env.REACT_APP_API_URL}/counter`);
+    const { data } = await instance.get(`/counter`);
     return data.value.value
 }
 
 export const postCount = async(newValue) => {
-    const {data : { value }} = await instance.post(`${process.env.REACT_APP_API_URL}/counter`, { newValue });
+    const {data : { value }} = await instance.post(`/counter`, { newValue });
     return value
 }
