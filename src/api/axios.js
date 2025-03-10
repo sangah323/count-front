2ee55beb558d05d9730e74b6fa87e86627e08c7b
imbhj <wnqudgus5565@gmail.com>
2025-03-11 08:27:49 +0900

    Fix: 250311 AWS 프론트엔드 코드 업로드

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`, // 환경 변수 활용
+  baseURL: `${process.env.REACT_APP_API_URL}`, // 환경 변수 활용
   headers: {
     'Content-Type': 'application/json',
   },
